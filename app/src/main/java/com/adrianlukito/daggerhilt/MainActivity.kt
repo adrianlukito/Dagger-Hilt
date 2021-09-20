e1ec29692dae435b3e7b0e12daac3c4f023254aa
adrianlukito <lukito.adrian1@gmail.com>
2021-09-20 11:29:18 +0700

    Field & Constructor Injection

--- app/src/main/java/com/adrianlukito/daggerhilt/MainActivity.kt
+++ app/src/main/java/com/adrianlukito/daggerhilt/MainActivity.kt
@@ -2,10 +2,42 @@ package com.adrianlukito.daggerhilt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    // field injection
+    @Inject
+    lateinit var someClass: SomeClass
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println(someClass.doAThing())
+        println(someClass.doSomeOtherThing())
+    }
+}
+
+// constructor injection
+class SomeClass @Inject constructor(
+    private val someOtherClass: SomeOtherClass
+) {
+
+    fun doAThing(): String {
+        return "Look I did a thing!"
+    }
+
+    fun doSomeOtherThing(): String {
+        return someOtherClass.doSomeOtherThing()
+    }
+}
+
+class SomeOtherClass @Inject constructor() {
+
+    fun doSomeOtherThing(): String {
+        return "Look I did some other thing!"
     }
 }
