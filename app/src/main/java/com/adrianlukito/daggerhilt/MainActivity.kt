9249a2a125182e899c86ae75243476243d85f7aa
adrianlukito <lukito.adrian1@gmail.com>
2021-09-21 11:57:31 +0700

    Modules, Binds, Provides

--- app/src/main/java/com/adrianlukito/daggerhilt/MainActivity.kt
+++ app/src/main/java/com/adrianlukito/daggerhilt/MainActivity.kt
@@ -2,10 +2,14 @@ package com.adrianlukito.daggerhilt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import com.google.gson.Gson
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.scopes.ActivityScoped
+import dagger.hilt.android.internal.managers.ApplicationComponentManager
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -29,15 +33,14 @@ class MainActivity : AppCompatActivity() {
 //Dependencies by specifying them as parameters to the class’s constructor.
 
 class SomeClass @Inject constructor(
-    private val someDependency: SomeInterface, // compile time error, cannot do constructor injection when injected interface
-    private val gson: Gson // compile time error, something not own, such as thrid party library
+    private val someInterfaceImpl: SomeInterface
 ){
     fun doAThing(): String{
-        return "Look I got: ${someDependency.getAThing()}"
+        return "Look I got: ${someInterfaceImpl.getAThing()}"
     }
 }
 
-class SomeDependency @Inject constructor() : SomeInterface {
+class SomeInterfaceImpl @Inject constructor() : SomeInterface {
     override fun getAThing() : String {
         return "A Thing"
     }
@@ -47,3 +50,44 @@ interface SomeInterface {
 
     fun getAThing(): String
 }
+
+// 2 option to inject interface/instance
+// @Provides
+// @Binds
+
+@InstallIn(SingletonComponent::class)
+@Module
+abstract class MyModule {
+
+//    PROVIDES
+    @Singleton
+    @Provides
+    fun provideSomeInterface (): SomeInterface {
+        return SomeInterfaceImpl()
+    }
+
+    @Singleton
+    @Provides
+    fun provideGson(): Gson {
+        return Gson()
+    }
+
+//    BINDS
+//    @Singleton
+//    @Binds
+//    abstract fun bindSomeDependency (
+//        someImpl: SomeInterfaceImpl
+//    ): SomeInterface
+//
+//    @Singleton
+//    @Binds
+//    abstract fun bindGson(
+//        gson: Gson
+//    ): Gson
+}
+
+
+
+
+
+
